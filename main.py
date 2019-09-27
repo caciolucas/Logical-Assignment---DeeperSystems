46bf7631cd7d450e9b466d08ad1cf6c0c5454543
caciolucas <cclucas060901@gmail.com>
2019-09-27 00:56:45 -0300

    Hotfix - Comparação com ultima mensagem

--- main.py
+++ main.py
@@ -25,25 +25,38 @@ for mensagem in mensagens:
 
 for usuario in usuarios:
     locals()['dicionario_%s' %usuario]={}
+    limite = 0
     for mensagem in locals()['usuario_%s' %usuario]:
         horario=float(mensagem['ts'])
         horarioemminutos=horario/60
-        ts=float(mensagem['ts'])
-        if locals()['usuario_%s' %usuario][0] == mensagem:
-            horarioatual=horario
+
+        if horarioemminutos>limite:
+            locals()['dicionario_%s' %usuario][horario]=[]
+            locals()['dicionario_%s' %usuario][horario].append(mensagem)
+            horarioant = horario=float(mensagem['ts'])
             limite=horarioemminutos+2
-            locals()['dicionario_%s' %usuario][ts]=[]
-            locals()['dicionario_%s' %usuario][ts].append(mensagem)
-        else:      
-            if horarioemminutos>limite and locals()['dicionario_%s' %usuario].keys():
-                locals()['dicionario_%s' %usuario][ts]=[]
-                horarioatual=horario
-                limite=horarioatual+2
-                locals()['dicionario_%s' %usuario][horarioatual].append(mensagem)
-            else:
-                locals()['dicionario_%s' %usuario][horarioatual].append(mensagem)
-                
+        else:
+            locals()['dicionario_%s' %usuario][horarioant].append(mensagem)
+
+
         
+##        horario=float(mensagem['ts'])
+##        horarioemminutos=horario/60
+##
+##        if horarioemminutos>limite:
+##            locals()['dicionario_%s' %usuario][horario]=[]
+##            locals()['dicionario_%s' %usuario][horario].append(mensagem)
+##            horarioant = horario=float(mensagem['ts'])
+##            limite=horarioemminutos+2
+##        else:
+##            if x==0:
+##                locals()['dicionario_%s' %usuario][horario]=[]
+##                horarioant = float(mensagem['ts'])
+##                limite=horarioemminutos+2
+##                x+=1
+##            locals()['dicionario_%s' %usuario][horarioant].append(mensagem)
+
+
 for usuario in usuarios:
     with open('%s.json'%usuario, 'w') as json_file:
         json.dump(locals()['dicionario_%s' %usuario], json_file, indent=2)
